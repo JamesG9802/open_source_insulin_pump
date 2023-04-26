fa58b6f5e165d3aafab77ca960bd3e707acc2637
JamesG9802 <61638683+JamesG9802@users.noreply.github.com>
2023-04-26 00:07:48 -0400

    Release build of APS system.
    Implemented build of APS system with integration with insulin pump.
    Refactored headers to be able to compile on stricter Arduino C++ environment.

--- APS Arduino/aps.ino/aps/APS_Glucose_Status.h
+++ APS Arduino/aps.ino/aps/APS_Glucose_Status.h
@@ -2,7 +2,13 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#ifndef __cplusplus
 typedef struct APS_Glucose_Status {
+#endif
+#ifdef __cplusplus
+struct Glucose_Status {
+#endif
 	double delta;
 	double glucose;
 	double long_avgdelta;
@@ -11,7 +17,17 @@ typedef struct APS_Glucose_Status {
 	double noise;
 	const char device[512];
 	double last_cal;			//	invalid if NAN
-} Glucose_Status;
+  #ifdef __cplusplus
+  Glucose_Status() : Glucose_Status(0, 0, 0, 0, 0, 0, 0, "", 0) {}
+  Glucose_Status(double a, double b, double c, double d, double e, long long f, double g, const char* h, double i) 
+  : delta(a), glucose(b), long_avgdelta(c), short_avgdelta(d), device(""), date(f), noise(g), last_cal(i) {}
+  Glucose_Status& operator=(const Glucose_Status& other) { return *this; }
+  #endif
+}
+#ifndef __cplusplus
+Glucose_Status
+#endif
+;
 #ifdef __cplusplus
 }
 #endif
