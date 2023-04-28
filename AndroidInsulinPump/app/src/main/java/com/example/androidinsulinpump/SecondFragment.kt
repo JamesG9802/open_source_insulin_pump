08179dfe0d70c7d1c79d4fa2f1ba228f8c50d018
LorenzoDitarantoNJIT <ld6@njit.edu>
2023-04-28 16:26:19 -0400

    added third fragment and changes

--- AndroidInsulinPump/app/src/main/java/com/example/androidinsulinpump/SecondFragment.kt
+++ AndroidInsulinPump/app/src/main/java/com/example/androidinsulinpump/SecondFragment.kt
@@ -35,6 +35,10 @@ class SecondFragment : Fragment() {
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+        binding.buttonNext.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_thirdFragment)
+        }
+
     }
 
     override fun onDestroyView() {
