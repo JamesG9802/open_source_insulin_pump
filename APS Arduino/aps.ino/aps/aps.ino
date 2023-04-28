3884b4a3cec7a3221422028d39ad60c813bb2161
mishlosh <mishlosh@gmail.com>
2023-04-28 19:34:44 -0400

    Added 2 characteristics

--- APS Arduino/aps.ino/aps/aps.ino
+++ APS Arduino/aps.ino/aps/aps.ino
@@ -28,6 +28,8 @@
 // BLE settings
 BLEService CGMData("180A");
 BLEByteCharacteristic switchCharacteristic("2A57", BLERead | BLEWrite);
+BLEByteCharacteristic aCharacteristic("a characteristic", BLERead | BLEWrite);
+BLEByteCharacteristic bCharacteristic("b Characteristic", BLERead | BLEWrite);
 
 //  CGM varaibles
 Glucose_Status* glucose_status;
@@ -232,10 +234,14 @@ void setup() {
   BLE.setAdvertisedService(CGMData);
 
   CGMData.addCharacteristic(switchCharacteristic);
+  CGMData.addCharacteristic(aCharacteristic);
+  CGMData.addCharacteristic(bCharacteristic);
 
   BLE.addService(CGMData);
 
   switchCharacteristic.writeValue(0);
+  aCharacteristic.writeValue(0);
+  bCharacteristic.writeValue(0);
 
   BLE.advertise();
 
@@ -264,6 +270,8 @@ void loop() {
           Serial.print("\nAnd is: ");
           Serial.print(glucose_status->glucose);
           Serial.print("\n----\n");
+          Serial.println(aCharacteristic.value());
+          Serial.println(bCharacteristic.value());
           
           /*  Clean up Temp so that it doesn't cause a memory leak  */
           Destroy_Temp(currenttemp);
