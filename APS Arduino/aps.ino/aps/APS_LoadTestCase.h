c66dfaf600175109e92ee2ddddb499eb98ee936c
JamesG9802 <61638683+JamesG9802@users.noreply.github.com>
2023-04-28 16:53:10 -0400

    Syntax fix

--- APS Arduino/aps.ino/aps/APS_LoadTestCase.h
+++ APS Arduino/aps.ino/aps/APS_LoadTestCase.h
@@ -5,6 +5,107 @@
 #include "APS_Autosens.h"
 #include "APS_Meal_Data.h"
 
+
+
+void TestCase0(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp) {
+	currenttemp->duration = 30;
+	currenttemp->rate = 1.5;
+}
+void TestCase1(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp) {
+	glucose_status->delta = -5;
+	glucose_status->glucose = 75;
+	glucose_status->long_avgdelta = -5;
+	glucose_status->short_avgdelta = -5;
+}
+void TestCase2(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp) {
+	currenttemp->duration = 57;
+	currenttemp->rate = 0;
+
+	glucose_status->delta = -5;
+	glucose_status->glucose = 75;
+	glucose_status->long_avgdelta = -5;
+	glucose_status->short_avgdelta = -5;
+}
+void TestCase3(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp) {
+	currenttemp->duration = 57;
+	currenttemp->rate = 0;
+	glucose_status->delta = 6;
+	glucose_status->glucose = 75;
+	glucose_status->long_avgdelta = 6;
+	glucose_status->short_avgdelta = 6;
+}
+void TestCase4(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp) {
+
+	glucose_status->delta = 6;
+	glucose_status->glucose = 75;
+	glucose_status->long_avgdelta = 6;
+	glucose_status->short_avgdelta = 6;
+}
+void TestCase5(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp) {
+
+	glucose_status->delta = 1;
+	glucose_status->glucose = 75;
+	glucose_status->long_avgdelta = 1;
+	glucose_status->short_avgdelta = 1;
+	iob_data->iob = -.05;
+	iob_data->activity = -.01;
+	iob_data->bolussnooze = 0;
+}
+void TestCase6(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp) {
+
+	glucose_status->delta = -1;
+	glucose_status->glucose = 75;
+	glucose_status->long_avgdelta = -1;
+	glucose_status->short_avgdelta = -1;
+	iob_data->iob = 1;
+	iob_data->activity = .01;
+	iob_data->bolussnooze = 0.5;
+}
+void TestCase7(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp) {
+
+	glucose_status->delta = 5;
+	glucose_status->glucose = 85;
+	glucose_status->long_avgdelta = 5;
+	glucose_status->short_avgdelta = 5;
+	iob_data->iob = -1;
+	iob_data->activity = -.01;
+	iob_data->bolussnooze = 0;
+}
+void TestCase8(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp) {
+
+	glucose_status->delta = 5;
+	glucose_status->glucose = 185;
+	glucose_status->long_avgdelta = 5;
+	glucose_status->short_avgdelta = 5;
+	iob_data->iob = 0;
+	iob_data->activity = -.01;
+	iob_data->bolussnooze = 0;
+}
+void TestCase9(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp) {
+
+	glucose_status->delta = 5;
+	glucose_status->glucose = 145;
+	glucose_status->long_avgdelta = 5;
+	glucose_status->short_avgdelta = 5;
+	currenttemp->duration = 180;
+	currenttemp->rate = 1.9;
+	iob_data->iob = 0;
+	iob_data->activity = -.01;
+	iob_data->bolussnooze = 0;
+}
+void TestCase10(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp) {
+
+	glucose_status->delta = 5;
+	glucose_status->glucose = 145;
+	glucose_status->long_avgdelta = 5;
+	glucose_status->short_avgdelta = 5;
+	currenttemp->duration = 30;
+	currenttemp->rate = 3.5;
+	iob_data->iob = 0;
+	iob_data->activity = -.01;
+	iob_data->bolussnooze = 0;
+}
+
 void LoadTestCase(Glucose_Status* g, IOB_Data* i, Temp* t, int testCaseNumber) {
 	switch (testCaseNumber)
 	{
@@ -43,103 +144,4 @@ void LoadTestCase(Glucose_Status* g, IOB_Data* i, Temp* t, int testCaseNumber) {
 		TestCase10(g, i, t);
 		break;
 	}
-}
-
-void TestCase0(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp, int testCaseNumber) {
-	currenttemp->duration = 30;
-	currenttemp->rate = 1.5;
-}
-void TestCase1(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp, int testCaseNumber) {
-	glucose_status->delta = -5;
-	glucose_status->glucose = 75;
-	glucose_status->long_avgdelta = -5;
-	glucose_status->short_avgdelta = -5;
-}
-void TestCase2(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp, int testCaseNumber) {
-	currenttemp.duration = 57;
-	currenttemp.rate = 0;
-
-	glucose_status->delta = -5;
-	glucose_status->glucose = 75;
-	glucose_status->long_avgdelta = -5;
-	glucose_status->short_avgdelta = -5;
-}
-void TestCase3(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp, int testCaseNumber) {
-	currenttemp.duration = 57;
-	currenttemp.rate = 0;
-	glucose_status->delta = 6;
-	glucose_status->glucose = 75;
-	glucose_status->long_avgdelta = 6;
-	glucose_status->short_avgdelta = 6;
-}
-void TestCase4(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp, int testCaseNumber) {
-
-	glucose_status->delta = 6;
-	glucose_status->glucose = 75;
-	glucose_status->long_avgdelta = 6;
-	glucose_status->short_avgdelta = 6;
-}
-void TestCase5(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp, int testCaseNumber) {
-
-	glucose_status->delta = 1;
-	glucose_status->glucose = 75;
-	glucose_status->long_avgdelta = 1;
-	glucose_status->short_avgdelta = 1;
-	iob_data->iob = -.05;
-	iob_data->activity = -.01;
-	iob_data->bolussnooze = 0;
-}
-void TestCase6(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp, int testCaseNumber) {
-
-	glucose_status->delta = -1;
-	glucose_status->glucose = 75;
-	glucose_status->long_avgdelta = -1;
-	glucose_status->short_avgdelta = -1;
-	iob_data->iob = 1;
-	iob_data->activity = .01;
-	iob_data->bolussnooze = 0.5;
-}
-void TestCase7(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp, int testCaseNumber) {
-
-	glucose_status->delta = 5;
-	glucose_status->glucose = 85;
-	glucose_status->long_avgdelta = 5;
-	glucose_status->short_avgdelta = 5;
-	iob_data->iob = -1;
-	iob_data->activity = -.01;
-	iob_data->bolussnooze = 0;
-}
-void TestCase8(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp, int testCaseNumber) {
-
-	glucose_status->delta = 5;
-	glucose_status->glucose = 185;
-	glucose_status->long_avgdelta = 5;
-	glucose_status->short_avgdelta = 5;
-	iob_data->iob = 0;
-	iob_data->activity = -.01;
-	iob_data->bolussnooze = 0;
-}
-void TestCase9(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp, int testCaseNumber) {
-
-	glucose_status->delta = 5;
-	glucose_status->glucose = 145;
-	glucose_status->long_avgdelta = 5;
-	glucose_status->short_avgdelta = 5;
-	*currenttemp->duration = 180;
-	currenttemp->rate = 1.9;
-	iob_data->iob = 0;
-	iob_data->activity = -.01;
-	iob_data->bolussnooze = 0;
-}
-void TestCase10(Glucose_Status* glucose_status, IOB_Data* iob_data, Temp* currenttemp, int testCaseNumber) {
-
-	glucose_status->delta = 5;
-	glucose_status->glucose = 145;
-	glucose_status->long_avgdelta = 5;
-	glucose_status->short_avgdelta = 5;
-	*currenttemp->duration = 30;
-	currenttemp->rate = 3.5;
-	iob_data->iob = 0;
-	iob_data->activity = -.01;
-	iob_data->bolussnooze = 0;
 }
