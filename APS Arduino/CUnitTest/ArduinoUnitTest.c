457282496e959cec46d4b5b2e23c420fd50d4525
JamesG9802 <61638683+JamesG9802@users.noreply.github.com>
2023-04-28 16:19:27 -0400

    Finishing Automated Testing for Hardware
    Arduino Communication Test added.
    Arduino Number transformation Test added.
    Insulin Pump Number conversion Test added.

--- APS Arduino/CUnitTest/ArduinoUnitTest.c
+++ APS Arduino/CUnitTest/ArduinoUnitTest.c
@@ -4,7 +4,7 @@
 
 /*	Insulin Pump Communication Test
 	
-	This checks whether the script's communication function works.
+	This checks whether the script's communication function works to send a signal to the insulin pump.
 */
 int TestPressButton(int buttonID, int expected)
 {
@@ -32,13 +32,7 @@ int TestPressButton(int buttonID, int expected)
         printf("Failed for %d\n", buttonID);
     return result == expected;
 }
-
-
-/*  This is the entry point to the testing code */
-int main()
-{
-	initArduinoLib();
-	setup();
+int Tests_for_PressButton() {
 
     int total = 6;
     int accuracy = 0;
@@ -49,6 +43,73 @@ int main()
     accuracy += TestPressButton(-1, 0);
     accuracy += TestPressButton(0, 0);
     accuracy += TestPressButton(255, 0);
-    printf("Results: %d/6", accuracy);
-    while (1);
+    printf("Results: %d/6\n", accuracy);
+    return accuracy == total;
+}
+/*  Send Number Pump Test
+    Checks if the function for transforming a number into a series of signals works.
+*/
+int TestSendNumberToPump(unsigned char number) {
+    int i;
+    int value = 0;
+    for (i = 7; i >= 0; i--) {
+        int mask = 1 << i;
+        value |= (mask & number);
+    }
+    if(number != value)
+    printf("Error: number = %d and value = %d\n", number, value);
+    return number == value;
+}
+int Tests_for_SendNumberToPump() {
+    int total = 6;
+    int accuracy = 0;
+    printf("Running Sending Number to Pump Tests\n");
+    accuracy += TestSendNumberToPump(0);
+    accuracy += TestSendNumberToPump(-10);
+    accuracy += TestSendNumberToPump(20);
+    accuracy += TestSendNumberToPump(255);
+    accuracy += TestSendNumberToPump(12);
+    accuracy += TestSendNumberToPump(-55);
+    printf("Results: %d/6\n", accuracy);
+    return accuracy == total;
+}
+/*  Insulin Pump Signal Reading Test
+    Checks if the function for the insulin pump to reinterpret signals into a number still works.
+*/
+int TestInsulinPumpCommunication(unsigned char number) {
+    int i;
+    for (i = 7; i >= 0; i--) {
+        int mask = 1 << i;
+        if ((number & mask) == mask) //  == 1
+            TIMER_0_task1_cb(1, 0, 0);
+        else  //  == 2
+            TIMER_0_task1_cb(0, 1, 0);
+    }
+    int result = number == TIMER_0_task1_cb(0, 0, 1);
+    button_array[0] = 0;
+    basal_rate[0] = -1;
+    return result;
+}
+int Tests_ForInsulinPumpCommunication() {
+    int total = 6;
+    int accuracy = 0;
+    printf("Running Insulin Pump Number Reading\n");
+    accuracy += TestSendNumberToPump(0);
+    accuracy += TestSendNumberToPump(-10);
+    accuracy += TestSendNumberToPump(20);
+    accuracy += TestSendNumberToPump(255);
+    accuracy += TestSendNumberToPump(12);
+    accuracy += TestSendNumberToPump(-55);
+    printf("Results: %d/6\n", accuracy);
+    return accuracy == total;
+}
+/*  This is the entry point to the testing code */
+int main()
+{
+
+    initArduinoLib();
+    int pressButtonResult = Tests_for_PressButton();
+    int sendNumberResult = Tests_for_SendNumberToPump();
+    int insulinPumpResult = Tests_ForInsulinPumpCommunication();
+    return !(pressButtonResult && sendNumberResult && insulinPumpResult);
 }
--- APS Arduino/CUnitTest/Source.h
+++ APS Arduino/CUnitTest/Source.h
@@ -8,23 +8,30 @@ This is the source Arduino code converted into C for testing.
 
 /*  Start of Arduino code   */
 
-/*  APS Button Pins for Arduino NANO 33 IoT */
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-#include "Arduino_Logic.h"
+#include "APS_Logic.h"
+#include "APS_Glucose_Status.h"
+#include "APS_Temperature.h"
+#include "APS_IOB_Data.h"
+#include "APS_Profile.h"
+#include "APS_Autosens.h"
+#include "APS_Meal_Data.h"
+#include "APS_basal-set-temp.h"
+#include "APS_round-basal.h"
+
+/*  APS Button Pins for Arduino NANO 33 IoT */
 
 #define SERIAL_TRANSMITTER 0
 #define SERIAL_RECEIVER 1
-#define APS_BUTTON1  6
-#define APS_BUTTON2  7
-#define APS_BUTTON3  18
+#define APS_BUTTON1  6  //  send a '1'
+#define APS_BUTTON2  7  //  send a '0'
+#define APS_BUTTON3  18 //  send command to deliver dosage
 
-/*  Initialize communication with Insulin Pump  */
-void InitInsulinPump() {
-    /*  Setup output direction for pins */
-    pinMode(APS_BUTTON1, OUTPUT_a);
-    pinMode(APS_BUTTON2, OUTPUT_a);
-    pinMode(APS_BUTTON3, OUTPUT_a);
-}
+#define INSULIN_INTERVAL  (1000 * 60 * 5) //  amount of time for how long it takes to recalculate required basal: default 5 min.
 
 /*  Take in a button ID and send a signal for .1s to Insulin Pump System (IPS)
     Does nothing if buttonID does not match existing pins.
@@ -42,20 +49,124 @@ void PressButton(unsigned char buttonID) {
         break;
     }
 }
+/*  An 8-bit number is translated into its individual bits to be sent to the pump
+    The bits are sent from right to left.
+*/
+void SendNumberToPump(unsigned char number) {
+    int i;
+    for (i = 7; i >= 0; i--) {
+        int mask = 1 << i;
+        if ((number & mask) == mask) //  == 1
+            PressButton(APS_BUTTON1);
+        else  //  == 2
+            PressButton(APS_BUTTON2);
+    }
+    PressButton(APS_BUTTON3);
+}
+/*  Given the current insulin dosage, calculate the appropriate units to send and tell the pump to deliver it.
+*/
+void SendCommandToPump(double rate, double duration) {
 
-/*  Initialize communication between Arduino and Continuous Glucose Monitor (CGM).  */
-void InitCGM() {
-    // Serial.begin(9600);
-}
-void setup() {
-    //InitInsulinPump();
-    //InitCGM();
-    pinMode(13, OUTPUT_a);
-}
-void loop() {
-    digitalWrite(13, HIGH);
-    delay(50);
-    digitalWrite(13, LOW);
-    delay(50);
+	double units;
+    //  Safety check to prevent erroneous output
+    if (isnan(rate) || isnan(duration) || rate == 0 || duration == 0)
+    {
+        return;
+    }
+    int insulinIntervalToMinutes = INSULIN_INTERVAL / 1000 / 60; // dividing to minutes
+    if (duration < insulinIntervalToMinutes) //  should finish by the time the next check occurs
+    {
+        //  units = number of 1/10th units insulin pump delivers for respective insulin interval
+        units = rate * 10 //  units per hr * 10 (1/10th units) per unit
+            / 60  //  1 hour per 60 minutes
+            * duration; // % active during insulin interval
+    }
+    else
+    {
+        //  units = number of 1/10th units insulin pump delivers for respective insulin interval
+        units = rate * 10 //  units per hr * 10 (1/10th units) per unit
+            / 60  //  1 hour per minute
+            * insulinIntervalToMinutes; // 100% active during insulin interval
+    }
+    unsigned char roundedUnits = (unsigned char)round(units);
+    SendNumberToPump(roundedUnits);
 }
 
+/*	This is the Insulin Pump Code converted for testing */
+unsigned char button_array[3]; 
+unsigned char basal_rate[1];
+void init_button_array()
+{
+	button_array[0] = 0;
+	button_array[1] = 0;
+	button_array[2] = 0;
+}
+
+int TIMER_0_task1_cb(unsigned char button_1, unsigned char button_2, unsigned char button_3)
+{
+	//	We also use button_array[0] to represent the insulinDeliveryAmount
+	unsigned char insulinDeliveryAmount = button_array[0];
+
+	//	To minimize changes, we reuse the unused basal_rate to represent the insulinDeliveryIndex
+	unsigned char insulinDeliveryIndex = basal_rate[0];
+
+	//	When Button 1 is pressed, the insulinDeliveryIndex is incremented by 1, insulinDelivery amount is left shifted by 1,
+	//	and a 1 is |ed to the insulinDeliveryAmount.
+	if (button_1)
+	{
+		/*
+		button_array[0]++;
+		button_1 = false;
+		timer_stop(&TIMER_0);
+		flash_erase(&FLASH_0, flash_address, 1);
+		store_delivery_data();
+		set_leds(button_array[0]);
+		*/
+		if (insulinDeliveryIndex >= 7)	//	insulinDeliveryAmount is 8 bit number and so trying to add more digits should do nothing
+			;
+		else
+		{
+			insulinDeliveryIndex++;
+			insulinDeliveryAmount <<= 1;
+			insulinDeliveryAmount |= 1;
+		}
+
+
+		//	Calling preexisting functions
+		button_array[0] = insulinDeliveryAmount;
+		basal_rate[0] = insulinDeliveryIndex;
+		return -1;
+	}
+	//	When Button 2 is pressed, the insulinDeliveryIndex is incremented by 1 and insulinDelivery amount is left shifted by 1.
+	else if (button_2)
+	{
+		/*
+		button_array[0]--;
+		button_2 = false;
+		timer_stop(&TIMER_0);
+		flash_erase(&FLASH_0, flash_address, 1);
+		store_delivery_data();
+		set_leds(button_array[0]);
+		*/
+		if (insulinDeliveryIndex >= 7)	//	insulinDeliveryAmount is 8 bit number and so trying to add more digits should do nothing
+			;
+		else
+		{
+			insulinDeliveryIndex++;
+			insulinDeliveryAmount <<= 1;
+		}
+
+		//	Calling preexisting functions
+		button_array[0] = insulinDeliveryAmount;
+		basal_rate[0] = insulinDeliveryIndex;
+		return -1;
+	}
+	//	When Button 3 is pressed, the main() function is called causing the insulinDelivery amount to be delivered.
+	//	insulinDeliveryIndex is then reset to -1.
+	else if (button_3)
+	{
+		button_array[2]++;
+		printf("Number of units: %d\n", insulinDeliveryAmount);
+		return insulinDeliveryAmount;
+	}
+}
